7ad67ec08fa991409af263653655f40534e222d5
Igor Halfeld <hello@igorluiz.me>
2024-04-30 18:34:19 -0300

    chore: another props to site config

---
+++ app.config.ts
@@ -0,0 +1,19 @@
+export default defineAppConfig({
+  name: "Igor's lucky notes",
+  description: "Build & learn in public notes",
+  host: "notes.igorhalfeld.com",
+
+  author: {
+    twitter: "igorhalfeld",
+  },
+
+  header: {
+    title: "Hi there 👋",
+    description:
+      "Welcome to my notes! here I share my thoughts, ideas, and learnings.",
+  },
+
+  ui: {
+    primary: "cyan",
+  },
+});
