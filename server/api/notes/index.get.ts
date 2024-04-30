c7155a1f24343d740fc310099e03bf475cf41a36
Igor Halfeld <hello@igorluiz.me>
2024-04-30 20:04:12 -0300

    feat: allow to see draft notes if admin

--- server/api/notes/index.get.ts
+++ server/api/notes/index.get.ts
@@ -1,15 +1,25 @@
-import { desc } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
-export default eventHandler(async () => {
+export default eventHandler(async (event) => {
   const db = useDatabase();
   if (!db) {
     throw createError({ statusCode: 500, message: "Database not available" });
   }
 
-  const notes = await db
+  const session = await getUserSession(event);
+  const loggedIn = Boolean(session.user)
+
+  const cursor = db
     .select()
     .from(tables.notes)
+
+  if (!loggedIn) {
+    cursor.where(eq(tables.notes.isDraft, loggedIn))
+  }
+    
+  const notes = await cursor
     .orderBy(desc(tables.notes.createdAt))
     .all();
+
   return notes;
 });
