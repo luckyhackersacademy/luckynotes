84447b5fc02d23a32546938b2cf301b0c9f28331
Igor Halfeld <hello@igorluiz.me>
2024-04-30 20:41:15 -0300

    feat: hero for note detail:

---
+++ components/NoteTitle.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import dayjs from "dayjs";
+
+const props = defineProps<{
+  title: string;
+}>();
+
+const time = computed(() => {
+  return dayjs(props.createdAt).format("LL");
+});
+</script>
+
+<template>
+  <div class="flex flex-col py-5">
+    <h1 class="font-bold text-2xl">{{ props.title }}</h1>
+    <p class="text-xl font-italic">Note written on {{ time }}</p>
+  </div>
+</template>
