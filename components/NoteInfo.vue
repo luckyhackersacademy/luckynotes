44899c76672810eac88e5a85e5257bb9e4a6461a
Igor Halfeld <hello@igorluiz.me>
2024-05-21 20:04:50 -0300

    feat: component to handle like and view

---
+++ components/NoteInfo.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  likes: number
+  views: number
+  alreadyLiked: boolean
+}>()
+
+const viewLabel = computed(() => {
+  if (props.views > 1) {
+    return `${props.views} views`
+  }
+
+  return `${props.views} view`
+})
+
+const likeLabel = computed(() => {
+  if (props.likes > 1) {
+    return `${props.likes} likes`
+  }
+
+  return `${props.likes} like`
+})
+</script>
+
+<template>
+  <div class="flex gap-2">
+    <UButton icon="i-heroicons-chart-bar" disabled color="cyan" variant="soft"> {{ viewLabel }} </UButton>
+    <UButton icon="i-heroicons-hand-thumb-up" :disabled="props.alreadyLiked" color="cyan" variant="soft">
+      {{ likeLabel }}
+    </UButton>
+  </div>
+</template>
