4514c9ba9919fdc1addfc9da2cfde484aa681537
Igor Halfeld <hello@igorluiz.me>
2024-06-04 11:48:35 -0300

    chore: change og-image to black bg

--- components/OgImage/Main.vue
+++ components/OgImage/Main.vue
@@ -1,24 +1,24 @@
 <script lang="ts" setup>
 const props = defineProps<{
-  title: string;
-  description: string;
-  host: string;
-}>();
+  title: string
+  description: string
+  host: string
+}>()
 </script>
 
 <template>
   <div
-    class="w-full h-full flex flex-col justify-between items-start bg-white p-20 pt-24 pb-20"
-    style="background-image: linear-gradient(135deg, #fff 0%, #eee 100%)"
+    class="w-full h-full flex flex-col justify-between items-start bg-black p-20 pt-24 pb-20"
+    style="background-image: linear-gradient(135deg, #000 0%, #444 100%)"
   >
     <div>
-      <p class="text-balance text-2xl mb-4 text-black flex items-center">
+      <p class="text-balance text-2xl mb-4 text-white flex items-center">
         {{ props.description }}
       </p>
-      <h1 class="text-5xl text-gray-700 leading-tight pr-10">
+      <h1 class="text-5xl text-white leading-tight pr-10">
         {{ props.title }}
       </h1>
     </div>
-    <div class="font-bold text-4xl flex flex-row">{{ props.host }}</div>
+    <div class="font-bold text-white text-4xl flex flex-row">{{ props.host }}</div>
   </div>
 </template>
